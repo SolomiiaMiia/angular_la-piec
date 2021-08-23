df38e288aded9978e0218afaeb103ad74ab952d9
Solomiia Brugitska <brigitska.solomiya@gmail.com>
2021-08-23 19:07:59 +0300

    add admin component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { DiscountComponent } from './pages/discount/discount.component';
+import { PizzaComponent } from './pages/pizza/pizza.component';
+import { SaladComponent } from './pages/salad/salad.component';
+import { DrinksComponent } from './pages/drinks/drinks.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { BasketComponent } from './pages/basket/basket.component';
+
+import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AdminDiscountsComponent } from './admin/admin-discounts/admin-discounts.component';
+import { AdminCategoriesComponent } from './admin/admin-categories/admin-categories.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+
+
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'discount', component: DiscountComponent },
+  { path: 'pizza', component: PizzaComponent },
+  { path: 'salad', component: SaladComponent },
+  { path: 'drinks', component: DrinksComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'basket', component: BasketComponent },
+
+  {
+    path: 'admin', component: AdminComponent, children: [
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      { path: 'dashboard', component: AdminDashboardComponent },
+      { path: 'discounts', component: AdminDiscountsComponent },
+      { path: 'categories', component: AdminCategoriesComponent },
+      { path: 'products', component: AdminProductsComponent },
+      { path: 'orders', component: AdminOrdersComponent },
+    ]
+  },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DiscountComponent } from './pages/discount/discount.component';
+import { PizzaComponent } from './pages/pizza/pizza.component';
+import { SaladComponent } from './pages/salad/salad.component';
+import { DrinksComponent } from './pages/drinks/drinks.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { BasketComponent } from './pages/basket/basket.component';
+
+import { AdminComponent } from './admin/admin.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AdminDiscountsComponent } from './admin/admin-discounts/admin-discounts.component';
+import { AdminCategoriesComponent } from './admin/admin-categories/admin-categories.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    DiscountComponent,
+    PizzaComponent,
+    SaladComponent,
+    DrinksComponent,
+    PaymentComponent,
+    BasketComponent,
+    AdminComponent,
+    AdminDashboardComponent,
+    AdminDiscountsComponent,
+    AdminCategoriesComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
   ],
   imports: [
     BrowserModule,
